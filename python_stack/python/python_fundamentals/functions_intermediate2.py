ff8b3debd3b6398e2eeffa1dad914042a7d658dc
Mutasem Siam <mutasemsiam4@gmail.com>
2022-09-24 09:57:42 +0300

    testing

---
+++ python_stack/python/python_fundamentals/functions_intermediate2.py
@@ -0,0 +1,72 @@
+#Question1
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+#1
+# x[1][0] = 15
+# print(x)
+
+#2
+# students[0]['last_name'] = 'Bryant'
+# print(students[0])
+
+#3
+# sports_directory['soccer'][0] = 'Andres'
+# print(sports_directory)
+
+#4
+# z[0]['y'] = 30
+# print(z)
+
+#Question2
+# def iterateDictionary(some_list):
+#     for x in some_list:
+#         for key,value in x.items():
+#             print(key,value)
+
+# students = [
+#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#          {'first_name' : 'John', 'last_name' : 'Rosales'},
+#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+# iterateDictionary(students)
+
+#Question3
+# def iterateDictionary2(key_name, some_list):
+#     for x in some_list:
+#         print(x[key_name])
+
+
+# students = [
+#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#          {'first_name' : 'John', 'last_name' : 'Rosales'},
+#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+
+# iterateDictionary2('last_name', students)
+
+#Question4
+# def printInfo(some_dict):
+#     for key, val in some_dict.items():
+        
+#         print(len(val), key)
+#         for x in val:
+#             print(x)
+
+# dojo = {
+#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+# printInfo(dojo)
+
+
