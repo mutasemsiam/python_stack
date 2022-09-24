f587bafbe6d3c84d88a9a99e9201881363c372ae
Mutasem Siam <mutasemsiam4@gmail.com>
2022-09-24 11:04:11 +0300

    creating django counter

---
+++ python_stack/django/django_intro/django_counter/app_one/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+def index(request):
+    if 'count' in request.session:
+        request.session['count'] += 1
+    else:
+        request.session['count'] = 0
+    return render(request, "index.html")
+
+
+def delete(request):
+
+    del request.session['count']
+
+    return redirect('/')
+
+def add(request):
+    if 'count' in request.session:
+        request.session['count'] += 2
+    else:
+        request.session['count'] = 0
+    return render(request, "index.html")
+
+
