d363a68fd82e323c78a1f23b55c37395af018233
Mutasem Siam <mutasemsiam4@gmail.com>
2022-09-25 15:23:54 +0300

    uploading ninja gold

---
+++ python_stack/django/django_intro/ninja_gold/app_ninja/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+import random
+from django import template
+from time import strftime, localtime
+    
+def index(request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0
+    if 'log' not in request.session:
+        request.session['log'] = []
+        
+    return render(request, "index.html")
+    
+def process(request):
+    logs= []
+    time = strftime("%b %d, %Y  %I:%M %p", localtime())
+    if request.POST['gold_game'] == 'gold_farm':
+        request.session['gold_found'] = random.randint(10,20)
+        request.session['gold'] += request.session['gold_found']
+        logs.append(f"You entered a farm and earned { request.session['gold_found']} gold ({time})")
+
+    if request.POST['gold_game'] == 'gold_cave':
+        request.session['gold_found'] = random.randint(10,20)
+        request.session['gold'] += request.session['gold_found']
+        logs.append(f"You entered a cave and earned { request.session['gold_found']} gold ({time})")
+
+    if request.POST['gold_game'] == 'gold_house':
+        request.session['gold_found'] = random.randint(10,20)
+        request.session['gold'] += request.session['gold_found']
+        logs.append(f"You entered a house and earned { request.session['gold_found']} gold ({time})")
+
+    if request.POST['gold_game'] == 'quest':
+        request.session['gold_found'] = random.randint(-50,50)
+        request.session['gold'] += request.session['gold_found']
+        if request.session['gold_found']<0:
+            logs.append(f"You failed a quest and lost { -request.session['gold_found']} gold ({time})")
+        else:
+            logs.append(f"You completed a quest and earned { request.session['gold_found']} gold ({time})")
+    
+
+    request.session['log'] += logs
+        
+    return redirect('/')
+
+def delete(request):
+    del request.session['gold']
+    del request.session['log']
+    return redirect('/')
+
+
+
